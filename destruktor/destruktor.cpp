3032576fcb4a0d54e30ea4112f9c2eb94f7ff4e4
foosaan <fauzan4297@gmail.com>
2023-05-17 13:51:01 +0700

    memanggil semua prosedur termasuk destructor dan constructor

--- destruktor/destruktor.cpp
+++ destruktor/destruktor.cpp
@@ -24,3 +24,23 @@ angka::~angka() { //destructor
 	delete[]arr;
 	cout << "alamat array sudah dilepas" << endl;
 }
+
+void angka::cetakData() {
+	for (int i = 1; i <= panjang;i++) {
+		cout << i << " = " << arr[i] << endl;
+	}
+}
+void angka::isiData() {
+	for (int i = 1; i <= panjang;i++) {
+		cout << i << " = "; cin >> arr[i];
+	}
+	cout << endl;
+}
+
+int main() {
+	angka belajarcpp(3); //constructor dipanggil
+	angka* ptrBelajarcpp = new angka(5); //constructor dipanggil
+	delete ptrBelajarcpp; //destructor dipanggil
+
+	return 0;
+} //destructor dipanggil
