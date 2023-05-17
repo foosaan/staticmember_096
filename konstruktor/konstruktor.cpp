5bc9eefb53b9de34cc69a8d1dc533f0d7ea8906c
foosaan <fauzan4297@gmail.com>
2023-05-17 13:52:02 +0700

    membuat prosedur mahasiswa

--- konstruktor/konstruktor.cpp
+++ konstruktor/konstruktor.cpp
@@ -33,17 +33,4 @@ void mahasiswa::cetak() {
 	cout << "Nama =" << nama << endl;
 }
 
-int main() {
-	mahasiswa mhs1;
-	mahasiswa mhs2(20);
-	mahasiswa mhs3("indra");
-	mahasiswa mhs4(30, "fauzan");
-
-	mhs1.cetak();
-	mhs2.cetak();
-	mhs3.cetak();
-	mhs4.cetak();
-
-	return 0;
-}
 
