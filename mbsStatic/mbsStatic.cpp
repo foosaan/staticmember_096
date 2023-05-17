af041ee4b98c9ef7ce2419ebf2b1960aa55d4cfc
foosaan <fauzan4297@gmail.com>
2023-05-17 11:11:52 +0700

    menambahakan fariabel mahasiwa

--- mbsStatic/mbsStatic.cpp
+++ mbsStatic/mbsStatic.cpp
@@ -4,4 +4,14 @@ using namespace std;
 
 class mahasiswa {
 public:
-	
+	static int nim;
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
+
+int mahasiswa::nim = 0;
+
